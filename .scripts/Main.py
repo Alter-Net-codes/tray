5c0fac98326ec70758c32eb04a688db6e94cf52a
T_cat9000_2 <terpstragraham@gmail.com>
2024-10-26 16:14:05 -0700

    update
    some errors need to be fixed

--- .scripts/Main.py
+++ .scripts/Main.py
@@ -1,10 +1,14 @@
+# imports
+import os
+
 # vars
 Branches = []
+Branch = 0
 LastBranch = 0
 ProjectName = 0
 Icon = 0
 Libs = []
-LibName
+LibName = 0
 LastLib = 0
 TimesLoopedBuildLibs = 0
 
@@ -14,9 +18,9 @@ print("please run this on a linux machine to build a poggit file.")
 # get branches
 while True:
   Branch = input("enter a branch to build from: ")
-  Branches.append(branch)
+  Branches.append(Branch)
   LastBranch = input("is that the last branch? [yes/no]: ")
-  if answer = "yes":
+  if LastBranch == "yes":
     print("okay!")
     break
   else:
@@ -29,23 +33,28 @@ ProjectName = input("enter the project name: ")
 Icon = input("enter icon file name including file extension: ")
 
 # get project libs
-while True
-  LibName = input("enter a lib: ")
-  LibSrc = input("enter the LibSrc: ")
-  LibVer = input("enter the LibVer: ")
-  Libs.append(LibName)
-  Libs.append(LibSrc)
-  Libs.append(LibVer)
-  LastLib = input("is that the last lib? [yes/no]: ")
-  if LastLib = "yes"
-    print("okay!")
-    break
-  else:
-    print()
+LibsTrue = input("are you intending to have libs? [yes/no]: ")
+if LibsTrue == "yes":
+    while True:
+        LibName = input("enter a lib: ")
+        LibSrc = input("enter the LibSrc: ")
+        LibVer = input("enter the LibVer: ")
+        Libs.append(LibName)
+        Libs.append(LibSrc)
+        Libs.append(LibVer)
+        LastLib = input("is that the last lib? [yes/no]: ")
+        if LastLib == "yes":
+            print("okay!")
+            break
+        else:
+            print()
+else:
+   print("okay!")
 
 # Build the Poggit File
-f = open("poggit.yml", "x")
-f = open("poggit.yml", "a")
+os.remove(".poggit.yml")
+f = open(".poggit.yml", "x")
+f = open(".poggit.yml", "a")
 f.write("---")
 # Build the branches section
 f.write("branches:")
@@ -58,11 +67,19 @@ f.write(f"  {ProjectName}:")
 f.write("    paths: ''")
 f.write("    icon: {icon}")
 # Build the libs
-f.write("    libs:")
-for i in range(int(len(Libs))):
-  f.write(f"    - {Libs[TimesLoopedBuildLibs]}\n")
-  f.write(f"      - {Libs[TimesLoopedBuildLibs] + 1}\n")
-  f.write(f"      - {Libs[TimesLoopedBuildLibs] + 2}\n")
-  TimesLoopedBuildLibs += 3
+if LibsTrue == "yes":
+    print(Libs)
+    f.write("    libs:")
+    while TimesLoopedBuildLibs < len(Libs):
+        # Check if there are enough elements remaining
+        if TimesLoopedBuildLibs < len(Libs):
+            f.write(f"    - {Libs[TimesLoopedBuildLibs]}\n")
+        if TimesLoopedBuildLibs + 1 < len(Libs):
+            f.write(f"      - {Libs[TimesLoopedBuildLibs + 1]}\n")
+        if TimesLoopedBuildLibs + 2 < len(Libs):
+            f.write(f"      - {Libs[TimesLoopedBuildLibs + 2]}\n")
+    
+    TimesLoopedBuildLibs += 3
+
 # write the end
 f.write("...")
